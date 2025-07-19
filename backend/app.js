1d604a1b433cbae44630dbfd7c742545cc4dd271
Vivek Sharma <mani1980rani@gmail.com>
2025-07-19 22:56:12 +0530

    endpoint correction

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(express.json());
-app.use('/api/users', authRoutes);
+app.use('/api/auth', authRoutes);
+// app.use('/api/users', authRoutes);
 // app.use(errorHandler); // global error handler
 
 module.exports = app;
